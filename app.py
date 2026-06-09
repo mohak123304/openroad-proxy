5bda26abb6eaca44ad49436deb05ee1fb65d20f3
mohak123304 <mohakjadhav20@gmail.com>
2026-06-09 18:59:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def read_root():
     return {"status": "Railway is running"}
 
 @app.get("/health")
-def health_check():
-    return {"status": "OK"}  # Codespace ko call mat kar yaha
+def health():
+    return {"status": "OK"}
